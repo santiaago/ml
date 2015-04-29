d5a49d6c2d6118455078abb3eb6f82ca5acc264f
santiaago <santiago.arias@red-gate.com>
2015-04-29 10:50:32 +0100

    add logreg initialize test

--- logreg/logreg_test.go
+++ logreg/logreg_test.go
@@ -7,3 +7,35 @@ func TestNewLogisticRegression(t *testing.T) {
 		t.Errorf("got nil linear regression")
 	}
 }
+
+func TestInitialize(t *testing.T) {
+	lr := NewLogisticRegression()
+	lr.Initialize()
+
+	if len(lr.Xn) != len(lr.Yn) {
+		t.Errorf("got different size of vectors Xn Yn, wants same size")
+	}
+
+	if len(lr.Xn[0]) != len(lr.Wn) {
+		t.Errorf("got different size of vectors Xn Wn, wants same size")
+	}
+
+	if len(lr.Xn) != lr.TrainingPoints {
+		t.Errorf("got different size of vectors Xn and training points, wants same number")
+	}
+
+	for i := 0; i < len(lr.Xn); i++ {
+		for j := 0; j < len(lr.Xn[0]); j++ {
+			if lr.Xn[i][j] < lr.Interval.Min ||
+				lr.Xn[i][j] > lr.Interval.Max {
+				t.Errorf("got value of Xn[%d][%d] = %v, want it between %v and %v", i, j, lr.Xn[i][j], lr.Interval.Min, lr.Interval.Max)
+			}
+		}
+	}
+
+	for i := 0; i < len(lr.Yn); i++ {
+		if lr.Yn[i] != float64(-1) && lr.Yn[i] != float64(1) {
+			t.Errorf("got value of Yn[%v] = %v, want it equal to -1 or 1", i, lr.Yn[i])
+		}
+	}
+}
