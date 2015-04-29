ef0a0439fde66fe1ff5336bc3445cb4fee20959d
santiaago <santiago.arias@red-gate.com>
2015-04-29 13:09:35 +0100

    logreg - add learn test

--- logreg/logreg_test.go
+++ logreg/logreg_test.go
@@ -49,6 +49,7 @@ func TestInitializeFromData(t *testing.T) {
 		{1, 1, 1},
 		{1, 1, 1},
 	}
+
 	lr := NewLogisticRegression()
 	if err := lr.InitializeFromData(data); err != nil {
 		t.Errorf("%v", err)
@@ -68,4 +69,37 @@ func TestInitializeFromData(t *testing.T) {
 	if len(lr.Xn[0]) != lr.VectorSize || len(data[0]) != lr.VectorSize {
 		t.Errorf("got difference in size of Xn[0] or data[0] with VectorSize")
 	}
+}
+
+func TestLearn(t *testing.T) {
+	lr := NewLogisticRegression()
+	data := [][]float64{
+		{0.1, 1, 1},
+		{0.2, 1, 1},
+		{0.3, 1, 1},
+		{1, 0.5, -1},
+		{1, 0.6, -1},
+		{1, 0.7, -1},
+	}
+
+	lr.InitializeFromData(data)
+	lr.Learn()
+	expectedWn := []float64{0.06586, -0.99194, 0.56851}
+	if !equal(expectedWn, lr.Wn) {
+		t.Errorf("Weight vector is not correct: got %v, want %v", lr.Wn, expectedWn)
+	}
+}
+
+const epsilon float64 = 0.001
+
+func equal(a, b []float64) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if (a[i] - b[i]) > epsilon {
+			return false
+		}
+	}
+	return true
 }
