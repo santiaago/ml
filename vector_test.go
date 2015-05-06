268696a64e366d56891efb42a51aaafa20d584a2
santiaago <santiago.arias@red-gate.com>
2015-05-06 11:31:23 +0100

    make dot test a table test

--- vector_test.go
+++ vector_test.go
@@ -3,14 +3,26 @@ package ml
 import "testing"
 
 func TestVectorDot(t *testing.T) {
-	var v Vector = []float64{1, 0, 0}
-	var u Vector = []float64{0, 1, 0}
-	want := float64(0)
-	if got, err := v.Dot(u); err != nil {
-		t.Errorf("Error computing Dot product")
-	} else {
-		if want != got {
-			t.Errorf("got %v, want %v", got, want)
+	tests := []struct {
+		v    Vector
+		u    Vector
+		want float64
+		err  string
+	}{
+		{
+			v:    []float64{1, 0, 0},
+			u:    []float64{0, 1, 0},
+			want: float64(0),
+		},
+	}
+
+	for i, tt := range tests {
+		if got, err := tt.v.Dot(tt.u); err != nil {
+			t.Errorf("Error computing Dot product")
+		} else {
+			if tt.want != got {
+				t.Errorf("test %v: got %v, want %v", i, got, tt.want)
+			}
 		}
 	}
 }
